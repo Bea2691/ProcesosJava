eef254594aafa360cd1ab739de45f41c49ee7f06
BeatrizRedondo <beatriz2691@gmail.com>
2024-11-13 17:14:19 +0100

    monitores

---
+++ ProgramacionProcesos/src/monitoresDeposito/Principal.java
@@ -0,0 +1,12 @@
+package monitoresDeposito;
+
+public class Principal {
+	  
+    public static void main(String[] args) {
+        
+        Monitor varCompartida=new Monitor();
+        
+        new Productor(varCompartida).start();
+        new Consumidor(varCompartida).start();
+    }
+}
